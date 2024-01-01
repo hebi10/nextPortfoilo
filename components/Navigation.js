18c31b94bc235a3112afe9014783ceeb565f626e
doyoung park <asdlkj0104@gmail.com>
2024-01-01 21:41:02 +0900

    Update 240101 01

--- components/Navigation.js
+++ components/Navigation.js
@@ -1,9 +1,14 @@
 import styleNav from '@/components/Navigation.module.css'
+import Link from 'next/link';
 
 export default function Navigation (){
     return (
-        <>
-            <p>Navigation</p>
-        </>
+        <ul className={styleNav.nav}>
+            <li><Link href='javascript:;'></Link></li>
+            <li><Link href='javascript:;'></Link></li>
+            <li><Link href='javascript:;'></Link></li>
+            <li><Link href='javascript:;'></Link></li>
+            <li><Link href='javascript:;'></Link></li>
+        </ul>
     );
 }
